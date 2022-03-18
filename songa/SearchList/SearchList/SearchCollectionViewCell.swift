0f017051bc00f0e5fdb9b6b136f1b9aedef0f471
asong57 <1998thddk@gmail.com>
2022-03-18 11:48:41 +0900

    Feat : 검색 리스트 기능 구현 완료

---
+++ songa/SearchList/SearchList/SearchCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  SearchCollectionViewCell.swift
+//  SearchList
+//
+//  Created by asong on 2022/03/18.
+//
+
+import UIKit
+
+class SearchCollectionViewCell: UICollectionViewCell{
+    @IBOutlet weak var searchLabel: UILabel!
+    @IBOutlet weak var deleteButton: UIButton!
+}
---
+++ songa/SearchList/SearchList/ViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ViewController.swift
+//  SearchList
+//
+//  Created by asong on 2022/03/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    var data = ["스위프트", "코로나", "엽떡","크리스피크림", "드라마"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as? SearchCollectionViewCell else{ return UICollectionViewCell() }
+        cell.searchLabel.text = data[indexPath.row]
+        cell.deleteButton.tag = indexPath.row
+        cell.deleteButton.addTarget(self, action: #selector(deleteButtonAction(sender:)), for: .touchUpInside)
+        return cell
+    }
+}
+
+extension ViewController {
+    @objc func deleteButtonAction(sender : UIButton) {
+        collectionView.deleteItems(at: [IndexPath.init(row: sender.tag, section: 0)])
+        data.remove(at: sender.tag)
+    }
+    
+    @IBAction func deleteAllData(){
+        data.removeAll()
+        collectionView.reloadData()
+    }
+}
+
+extension ViewController: UISearchBarDelegate{
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        if let  text = searchBar.text {
+            data.insert(text, at: 0)
+            collectionView.reloadData()
+        }
+    }
+}
