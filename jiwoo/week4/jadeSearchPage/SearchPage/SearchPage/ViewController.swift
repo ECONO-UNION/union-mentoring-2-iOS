efb7bc9ede49a194562bfe21af5827aefdda45ad
earlysummer0303 <jw9914@naver.com>
2022-04-02 05:36:07 +0900

    searchbar 하는중

---
+++ jiwoo/week4/jadeSearchPage/SearchPage/SearchPage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SearchPage
+//
+//  Created by 황지우 on 2022/04/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
