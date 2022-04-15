cb7e2bb513aa289ab99211f8ba97dbdde7757bba
seungbong88 <seungbong8.8@gmail.com>
2022-04-16 00:07:03 +0900

    initial commit

--- seunghee/ImageCachingProject/ImageCachingProject/ImageCachingProject/ViewController.swift
+++ seunghee/ImageCachingProject/ImageCachingProject/ImageCachingProject/ViewController.swift
@@ -6,12 +6,17 @@
 //
 
 import UIKit
+import Kingfisher
 
 class ViewController: UIViewController {
   
   @IBOutlet weak var collectionView: UICollectionView!
   
+  // https://picsum.photos/v2/list
   let imageDownloadUrl = "https://picsum.photos/640/360/?random"
+  
+  var randomNum = Int.random(in: 0...9)
+
   
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -22,6 +27,7 @@ class ViewController: UIViewController {
   private func configureView() {
     collectionView.delegate = self
     collectionView.dataSource = self
+    collectionView.prefetchDataSource = self
     
     let flowLayout = UICollectionViewFlowLayout()
     flowLayout.minimumLineSpacing = 0.0
@@ -33,11 +39,14 @@ class ViewController: UIViewController {
   
   @IBAction func buttonDidTap(_ sender: Any) {
     ImageCache.clear()
+    randomNum = Int.random(in: 0...9)
+    collectionView.setContentOffset(.zero, animated: true)
     collectionView.reloadData()
   }
 }
 
-extension ViewController: UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
+extension ViewController: UICollectionViewDelegateFlowLayout, UICollectionViewDataSource, UICollectionViewDataSourcePrefetching {
+  
   func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
     return 20
   }
@@ -48,7 +57,24 @@ extension ViewController: UICollectionViewDelegateFlowLayout, UICollectionViewDa
     }
     
     cell.updateUI(index: indexPath.row, imageUrl: imageDownloadUrl)
-    
+    cell.imageView.image = UIImage()
+
+//    let urlString = "https://picsum.photos/id/\(String(indexPath.row))\(randomNum)/640/360"
+//    if let url = URL(string: urlString) {
+//      let cacheKey = urlString //imageDownloadUrl + String(indexPath.row)
+//      let resource = ImageResource(downloadURL: url, cacheKey: cacheKey)
+//      let processor = RoundCornerImageProcessor(cornerRadius: 20)
+//
+//      print("cache key : \(cacheKey)")
+//      cell.imageView.kf.indicatorType = .activity
+//      cell.imageView.kf.setImage(
+//        with: resource,
+//        options: [
+//          .transition(.fade(1)),
+//          .processor(processor)
+//        ])
+//    }
+      
     return cell
   }
   
@@ -56,5 +82,19 @@ extension ViewController: UICollectionViewDelegateFlowLayout, UICollectionViewDa
     let screenWidth = UIScreen.main.bounds.width
     let ratio: Double = 360.0 / 640.0
     return CGSize(width: screenWidth, height: screenWidth * ratio)
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, prefetchItemsAt indexPaths: [IndexPath]) {
+    for indexPath in indexPaths {
+              print("prefetch >> \(indexPath.row)")
+      ImageDownloader.shared.downloadImage(urlString: imageDownloadUrl,
+                                           index: indexPath.row,
+                                           completion: { image in
+
+        DispatchQueue.main.async {
+          collectionView.reloadItems(at: [indexPath])
+        }
+      })
+    }
   }
 }
---
+++ seunghee/PokemonProject/PokemonProject/PokemonProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokemonProject
+//
+//  Created by seungbong on 2022/04/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
